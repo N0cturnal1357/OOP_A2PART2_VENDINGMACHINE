10129a182c689c1c18549a7f10e807d6f4550a7d
JD-0631 <jdaley5@mycambrian.ca>
2026-02-14 21:54:34 -0500

    Added: -sprites -VendingMachine class -state variables -constructor
    Started:
    -Accessor Methods
    -Mutator Methods

--- OOP_A2PART2_VENDINGMACHINE/Game1.cs
+++ OOP_A2PART2_VENDINGMACHINE/Game1.cs
@@ -9,6 +9,21 @@ namespace OOP_A2PART2_VENDINGMACHINE
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //background
+        private Texture2D bg;
+
+        //sprite
+        private Texture2D vmSprite;
+        private Vector2 vmPosition;
+        private int vmScale;
+
+        //hitbox
+        private Rectangle hitbox;
+        //whether the mouse is currently over the vending machine
+        private bool isHovering;
+        //1x1 texture used to draw hitboxes/debug rectangles
+        private Texture2D pixel;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,6 +34,25 @@ namespace OOP_A2PART2_VENDINGMACHINE
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            //screen size
+            _graphics.PreferredBackBufferWidth = 1600;
+            _graphics.PreferredBackBufferHeight = 800;
+            _graphics.ApplyChanges();
+
+            //background
+            bg = Content.Load<Texture2D>("CityBG");
+
+            //sprite
+            vmSprite = Content.Load<Texture2D>("VendingMachine");
+
+            //sprite position
+            vmScale = 5;
+            int vmX = (GraphicsDevice.Viewport.Width - vmSprite.Width*vmScale) / 2;
+            int vmY = (GraphicsDevice.Viewport.Height - vmSprite.Height*vmScale) / 2;
+            vmPosition = new Vector2(vmX, vmY);
+
+            //hitbox
+            hitbox = new Rectangle(vmX, vmY, vmSprite.Width * vmScale, vmSprite.Height * vmScale);
 
             base.Initialize();
         }
@@ -26,6 +60,10 @@ namespace OOP_A2PART2_VENDINGMACHINE
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // create a 1x1 white texture for drawing rectangles / hitboxes
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
 
             // TODO: use this.Content to load your game content here
         }
@@ -36,6 +74,9 @@ namespace OOP_A2PART2_VENDINGMACHINE
                 Exit();
 
             // TODO: Add your update logic here
+            // check mouse hover over vending machine
+            var mouse = Mouse.GetState();
+            isHovering = hitbox.Contains(mouse.X, mouse.Y);
 
             base.Update(gameTime);
         }
@@ -45,8 +86,40 @@ namespace OOP_A2PART2_VENDINGMACHINE
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
 
+            //background
+            _spriteBatch.Draw(bg, new Vector2(0, -495), null, Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0f);
+
+            //sprite
+            _spriteBatch.Draw(vmSprite, vmPosition, null, Color.White, 0f, Vector2.Zero, vmScale, SpriteEffects.None, 0f);
+
+            //hitbox
+            // show hitbox outline when hovering (alpha > 0) otherwise hide
+            DrawCollision(hitbox.Width, hitbox.Height, 2, Color.Red, isHovering ? 0.25f : 0f);
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
+
+        private void DrawCollision(int width, int height, int thickness, Color color, float alpha)
+        {
+            //hitbox
+            // draw a semi-transparent fill so the hitbox area is visible
+            _spriteBatch.Draw(pixel, hitbox, Color.Red * alpha);
+
+            if (alpha != 0f)
+            {
+                // top
+                _spriteBatch.Draw(pixel, new Rectangle(hitbox.Left, hitbox.Top, hitbox.Width, thickness), Color.Red);
+                // bottom
+                _spriteBatch.Draw(pixel, new Rectangle(hitbox.Left, hitbox.Bottom - thickness, hitbox.Width, thickness), Color.Red);
+                // left
+                _spriteBatch.Draw(pixel, new Rectangle(hitbox.Left, hitbox.Top, thickness, hitbox.Height), Color.Red);
+                // right
+                _spriteBatch.Draw(pixel, new Rectangle(hitbox.Right - thickness, hitbox.Top, thickness, hitbox.Height), Color.Red);
+            }
+        }
+
     }
 }
---
+++ OOP_A2PART2_VENDINGMACHINE/VendingMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_A2PART2_VENDINGMACHINE
+{
+    internal class VendingMachine
+    {
+        private int hits;
+        private int health;
+        private int price;
+        private int moneyAdded;
+        private bool containsItem;
+        private string[] items;
+        private string item;
+        public void createVendingMachine(bool containsItem = false, string item = "Nothing")
+        {
+            hits = 0;
+            moneyAdded = 0;
+            health = new Random().Next(10, 50);
+
+
+
+
+
+
+            this.containsItem = containsItem;
+            if (containsItem)
+            {
+                setItem();
+            }
+        }
+
+        public void setItem()
+        {
+            items = ["Chips", "Soda", "Candy"];
+            int random = new Random().Next(0, items.Length);
+
+            item = items[random];
+        }
+
+        public void setPrice(int min, int max)
+        {
+            price = new Random().Next(min, max);
+        }
+    }
+}
