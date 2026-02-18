a088144588cf84eb64ccd0f29554227645c29375
JD-0631 <jdaley5@mycambrian.ca>
2026-02-18 14:07:26 -0500

    Finished buttons and mutator methods

--- OOP_A2PART2_VENDINGMACHINE/Game1.cs
+++ OOP_A2PART2_VENDINGMACHINE/Game1.cs
@@ -24,37 +24,29 @@ namespace OOP_A2PART2_VENDINGMACHINE
 
         //sprite
         private Texture2D vmSprite;
-        private Vector2 vmPosition;
-        private int vmScale;
-        //machine hitbox
-        private Rectangle hitbox;
-        //whether the mouse is currently over the vending machine
-        private bool vmHovering;
+
         //1x1 texture used to draw hitboxes/debug rectangles
         private Texture2D pixel;
 
         //buttons===============================================================
+        private Rectangle bGreenHitbox;
+        private Rectangle bRedHitbox;
+        private bool isHoveringGreen;
+        private bool isHoveringred;
+        private int buttonScale;
+
         //pay button
         private Texture2D bGreenUp;
         private Texture2D bGreenDown;
-        private bool bIsPressed;
-        //button hovering
-        private bool bHovering;
-        //button hitbox
-        private Rectangle bHitbox;
-        //button scale
-        private int bScale;
-        //button position
-        private Vector2 bPosition;
-        //button text
-        private string bGreenText = "PAY";
-        private Vector2 bGreenTextPos;
+        private Texture2D bGreenCurrentSprite;
+        private Vector2 bGreenPosition;
+        private Vector2 greenTextPosition;
 
         //kick button
-
-
-
-
+        private Texture2D bRedUp;
+        private Texture2D bRedDown;
+        private Texture2D bRedCurrentSprite;
+        private Vector2 redTextPosition;
 
         public Game1()
         {
@@ -70,14 +62,13 @@ namespace OOP_A2PART2_VENDINGMACHINE
             _graphics.PreferredBackBufferHeight = 800;
             _graphics.ApplyChanges();
 
-            vmScale = 5;
-            bScale = 5;
-
             font = Content.Load<SpriteFont>("Font");
 
             vm = new VendingMachine();
             vm.CreateVendingMachine();
 
+            buttonScale = 5;
+           
             base.Initialize();
         }
 
@@ -98,6 +89,8 @@ namespace OOP_A2PART2_VENDINGMACHINE
             //button sprite
             bGreenUp = Content.Load<Texture2D>("buttonFrames/buttonGreenUp");
             bGreenDown = Content.Load<Texture2D>("buttonFrames/buttonGreenDown");
+            bRedUp = Content.Load<Texture2D>("buttonFrames/buttonRedUp");
+            bRedDown = Content.Load<Texture2D>("buttonFrames/buttonRedDown");
         }
 
         protected override void Update(GameTime gameTime)
@@ -108,49 +101,59 @@ namespace OOP_A2PART2_VENDINGMACHINE
             //mouse
             var mouse = Mouse.GetState();
 
-            //machine position
-            int vmX = (GraphicsDevice.Viewport.Width - vmSprite.Width * vmScale) / 2;
-            int vmY = (GraphicsDevice.Viewport.Height - vmSprite.Height * vmScale) / 2;
-            vmPosition = new Vector2(vmX, vmY);
+            //button text position
+            greenTextPosition.X = GraphicsDevice.Viewport.Width / 2 - (bGreenUp.Width * buttonScale) - 85;
+            redTextPosition.X = GraphicsDevice.Viewport.Width / 2 + 215;
 
-            //machine hitbox
-            hitbox = new Rectangle(vmX, vmY, vmSprite.Width * vmScale, vmSprite.Height * vmScale);
-            vmHovering = hitbox.Contains(mouse.X, mouse.Y);
+            //button hitboxes
+            //pay button
+            bGreenHitbox = new Rectangle
+            (
+                GraphicsDevice.Viewport.Width / 2 - (bGreenUp.Width * buttonScale) - 150,
+                GraphicsDevice.Viewport.Height - (bGreenUp.Height * buttonScale) - 50,
+                bGreenUp.Width * buttonScale,
+                bGreenUp.Height * buttonScale
+            );
+            //kick button
+            bRedHitbox = new Rectangle
+            (
+                GraphicsDevice.Viewport.Width / 2 + 150,
+                GraphicsDevice.Viewport.Height - (bRedUp.Height * buttonScale) - 50,
+                bRedUp.Width * buttonScale,
+                bRedUp.Height * buttonScale
+            );
 
-            //button position
-            int bX = (GraphicsDevice.Viewport.Width - bGreenUp.Width * bScale) / 2 - 100;
-            int bY = GraphicsDevice.Viewport.Height - bGreenUp.Height * bScale - 50;
-            bPosition = new Vector2(bX, bY);
-            
-            
-
-            //button hitbox
-            bHitbox = new Rectangle((int)bPosition.X, (int)bPosition.Y, bGreenUp.Width * bScale, bGreenUp.Height * bScale);
-            bHovering = bHitbox.Contains(mouse.X, mouse.Y);
-
-            //BUTTON FUNCTIONALITY HERE
-            if (bHovering && mouse.LeftButton == ButtonState.Pressed)
+            isHoveringGreen = bGreenHitbox.Contains(mouse.Position);
+            isHoveringred = bRedHitbox.Contains(mouse.Position);
+            if (isHoveringGreen && mouse.LeftButton == ButtonState.Pressed)
             {
-                bIsPressed = true;
-                bGreenTextPos = new Vector2(bPosition.X + (bGreenUp.Width * bScale) / 2 - font.MeasureString(bGreenText).X / 2, bPosition.Y + 32);
+                bGreenCurrentSprite = bGreenDown;
+                vm.StartGive(Color.Green);
+                greenTextPosition.Y = GraphicsDevice.Viewport.Height - (bGreenUp.Height * buttonScale) - 15;
             }
+            if (isHoveringred && mouse.LeftButton == ButtonState.Pressed)
+            {
+                bRedCurrentSprite = bRedDown;
+                vm.StartKick(Color.Red);
+                redTextPosition.Y = GraphicsDevice.Viewport.Height - (bRedUp.Height * buttonScale) - 15;
+            }
+            
             if (mouse.LeftButton == ButtonState.Released)
             {
-                // If the button was pressed (press started inside the button),
-                // trigger the vending machine give-money animation.
-                if (bIsPressed)
-                {
-                    vm.StartGive(new Vector2(vmPosition.X + vmSprite.Width * vmScale + 10, vmPosition.Y), Color.LimeGreen);
-                }
-
-                bIsPressed = false;
-                bGreenTextPos = new Vector2(bPosition.X + (bGreenUp.Width * bScale) / 2 - font.MeasureString(bGreenText).X / 2, bPosition.Y + 15);
+                bGreenCurrentSprite = bGreenUp;
+                bRedCurrentSprite = bRedUp;
+                greenTextPosition.Y = GraphicsDevice.Viewport.Height - (bGreenUp.Height * buttonScale) - 25;
+                redTextPosition.Y = GraphicsDevice.Viewport.Height - (bRedUp.Height * buttonScale) - 25;
             }
+
+            //machine position
+            vm.SetPosition(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, vmSprite);
 
             // update vending machine animations
             vm.UpdateGive();
 
-
+            // update kick animation
+            vm.UpdateKick();
 
             // stats drawing handled in Draw()
             base.Update(gameTime);
@@ -165,30 +168,57 @@ namespace OOP_A2PART2_VENDINGMACHINE
             //background
             _spriteBatch.Draw(bg, new Vector2(0, -495), null, Color.White, 0f, Vector2.Zero, 1, SpriteEffects.None, 0f);
 
+            //buttons
+            //pay button
+            _spriteBatch.Draw
+            (
+                bGreenCurrentSprite, 
+                new Vector2
+                (
+                    GraphicsDevice.Viewport.Width/2 - (bGreenUp.Width * buttonScale) - 150, 
+                    GraphicsDevice.Viewport.Height - (bGreenUp.Height * buttonScale) - 50
+                ), 
+                null, 
+                Color.White, 
+                0f, 
+                Vector2.Zero, 
+                buttonScale, 
+                SpriteEffects.None, 
+                0f
+            );
+            _spriteBatch.DrawString(font, "PAY", greenTextPosition, Color.DarkGreen);
+            //kick button
+            _spriteBatch.Draw
+            (
+                bRedCurrentSprite,
+                new Vector2
+                (
+                   GraphicsDevice.Viewport.Width / 2 + 150,
+                    GraphicsDevice.Viewport.Height - (bRedUp.Height * buttonScale) - 50
+                ),
+                null,
+                Color.White,
+                0f,
+                Vector2.Zero,
+                buttonScale,
+                SpriteEffects.None,
+                0f
+            );
+            _spriteBatch.DrawString(font, "KICK", redTextPosition, Color.DarkRed);
+
             //sprite
-            _spriteBatch.Draw(vmSprite, vmPosition, null, Color.White, 0f, Vector2.Zero, vmScale, SpriteEffects.None, 0f);
-
-            //hitbox
-            //show hitbox outline when hovering (debug)
-            DrawCollision(hitbox, hitbox.Width, hitbox.Height, 2, Color.Red, vmHovering ? 0.25f : 0f);
-
-            //draw button
-            if (bIsPressed) _spriteBatch.Draw(bGreenDown, bPosition, null, Color.White, 0f, Vector2.Zero, bScale, SpriteEffects.None, 0f);
-            else _spriteBatch.Draw(bGreenUp, bPosition, null, Color.White, 0f, Vector2.Zero, bScale, SpriteEffects.None, 0f);
-            //draw button text
-            _spriteBatch.DrawString(font, bGreenText, bGreenTextPos, Color.DarkGreen);
-
-            //show button hitbox when hovering (debug)
-            DrawCollision(bHitbox, bHitbox.Width, bHitbox.Height, 2, Color.Green, bHovering ? 0.25f : 0f);
-
+            //_spriteBatch.Draw(vmSprite, vmPosition, null, Color.White, 0f, Vector2.Zero, vmScale, SpriteEffects.None, 0f);
+            vm.DrawMachine(_spriteBatch);
 
             // Draw give-money text if the vending machine is animating it.
-            vm.GiveMoney(_spriteBatch, font, new Vector2(vmPosition.X - 20, vmPosition.Y), Color.Green);
+            vm.GiveMoney(_spriteBatch, font, Color.Green);
+
+            //same with kick animation
+            vm.Kick(_spriteBatch, font, Color.Red);
 
             // Draw vending machine stats
             string stats = vm.GetStats();
             _spriteBatch.DrawString(font, stats, new Vector2(10, 10), Color.White);
-
 
             _spriteBatch.End();
             base.Draw(gameTime);
--- OOP_A2PART2_VENDINGMACHINE/VendingMachine.cs
+++ OOP_A2PART2_VENDINGMACHINE/VendingMachine.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace OOP_A2PART2_VENDINGMACHINE
 {
@@ -27,6 +30,22 @@ namespace OOP_A2PART2_VENDINGMACHINE
         private float giveRisePerFrame = 0.5f;
         private float giveFadePerFrame = 0.02f;
 
+        //kick animation state
+        private bool isKicking;
+        private Vector2 kickPosition;
+        private Color kickColor;
+        private string kickText;
+        private float kickAlpha;
+        private float kickRisePerFrame = 0.5f;
+        private float kickFadePerFrame = 0.02f;
+
+        private Vector2 position;
+        private Texture2D vmSprite;
+        private int vmScale;
+
+        //items gained
+        List<string> itemsGained = new List<string>();
+
         public void CreateVendingMachine()
         {
             hits = 0;
@@ -36,10 +55,23 @@ namespace OOP_A2PART2_VENDINGMACHINE
             SetPrice();
 
             min = 1;
-            max = 100; //1-100 chance to get item
+            max = 12 * health;
         }
 
-        // Return a stats string so drawing can be done from the game's Draw() method
+        public void SetPosition(int x, int y, Texture2D sprite)
+        {
+            vmSprite = sprite;
+            vmScale = 5;
+            //machine position
+            int vmX = (x - vmSprite.Width * vmScale) / 2;
+            int vmY = (y - vmSprite.Height * vmScale) / 2;
+            position = new Vector2(vmX, vmY);
+        }
+
+        public void DrawMachine(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(vmSprite, position, null, Color.White, 0f, Vector2.Zero, vmScale, SpriteEffects.None, 0f);
+        }
         public string GetStats()
         {
             string stats = 
@@ -50,15 +82,21 @@ namespace OOP_A2PART2_VENDINGMACHINE
                 $"Total Money Given: {totalMoneyGiven}\n" +
                 $"Contains Item: {containsItem}\n" +
                 $"Item: {item}\n" +
-                $"Chance to Get Item: {min}/{max}%" 
+                $"Chance to Get Item: {chance} / {max}\n" +
+                $"Items Gained: {string.Join(", ", itemsGained)}"
                 ;
 
             return stats;
         }
 
-        public void SetPrice(int min = 1, int max = 11) //1-10
+        public void SetPrice(int min = 1, int max = 6) //1-5 default range
         {
             price = new Random().Next(min, max);
+        }
+
+        public int GetPrice()
+        {
+            return price;
         }
 
         public void SetItem()
@@ -77,23 +115,53 @@ namespace OOP_A2PART2_VENDINGMACHINE
         public void GiveItem()
         {
             if (!containsItem) return;
-
+            itemsGained.Add(item);
             SetItem();
             SetPrice();
         }
 
-        public void Kick()
+        //KICK STUFF=====================================================================
+        public void Kick(SpriteBatch spriteBatch, SpriteFont font, Color color)
         {
+            if (!isKicking) return;
+
+            spriteBatch.DrawString(font, kickText, kickPosition, kickColor * kickAlpha, 0f, Vector2.Zero, 2f, SpriteEffects.None, 1f);
+        }
+        public void UpdateKick()
+        {
+           if (!isKicking) return;
+            kickPosition.Y -= kickRisePerFrame;
+            kickAlpha -= kickFadePerFrame;
+            if (kickAlpha <= 0f)
+            {
+                kickAlpha = 0f;
+                isKicking = false;
+            }
+        }
+
+        public void StartKick(Color color)
+        {
+            if (isKicking) return; // already running
+
+            health -= damage;
+            SetChance();
+            kickText = $"- {damage}";
+            kickPosition = new Vector2(position.X + vmSprite.Width*vmScale + 30, position.Y);
+            kickColor = color;
+            kickAlpha = 1f;
+            isKicking = true;
+
             hits++;
             damage = new Random().Next(1, 10);
         }
+
+        //GIVE MONEY STUFF================================================================
         public void GetMoney(SpriteBatch spriteBatch, SpriteFont font, Vector2 position, Color color)
         {
             string totalGiven = $"Money Added: {this.totalMoneyGiven}";
             spriteBatch.DrawString(font, totalGiven, position, color);
         }
-        //GIVE MONEY STUFF================================================================
-        public void GiveMoney(SpriteBatch spriteBatch, SpriteFont font, Vector2 position, Color color)
+        public void GiveMoney(SpriteBatch spriteBatch, SpriteFont font, Color color)
         {
             //Draw the current give-money animation state
             if (!isGiving) return;
@@ -102,13 +170,13 @@ namespace OOP_A2PART2_VENDINGMACHINE
         }
 
         //Start the give-money animation (called when the button is pressed)
-        public void StartGive(Vector2 position, Color color)
+        public void StartGive(Color color)
         {
             if (isGiving) return; // already running
 
             totalMoneyGiven += price;
             giveText = $"+ {price}";
-            givePosition = position;
+            givePosition = new Vector2(position.X - 30, position.Y);
             giveColor = color;
             giveAlpha = 1f;
             isGiving = true;
@@ -116,9 +184,8 @@ namespace OOP_A2PART2_VENDINGMACHINE
             //change to give item, if there is one
             if (containsItem)
             {
-                //1 in 100 chance to recieve what is in the machine (no guarentee to be what you wanted tho)
-                chance = new Random().Next(min, max+1);
-                if (chance == max)
+                int randNum = new Random().Next(min, max + 1);
+                if (randNum <= chance)
                 {
                     GiveItem();
                 }
@@ -139,6 +206,14 @@ namespace OOP_A2PART2_VENDINGMACHINE
                 isGiving = false;
                 SetPrice(); //Set a new price for the next purchase
             }
+        }
+
+        //CHANCE TO GET ITEM STUFF================================================================
+        private void SetChance()
+        {
+            min = 1;
+            max = 12 * health; //higher health = lower chance to get item
+            chance = new Random().Next(min, max + 1);
         }
     }
 }
